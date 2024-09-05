d7b6222c33b5a6f4b24a2b836414ea0dc25861b3
MarcoSac1 <marcosaccone8@gmail.com>
2024-09-05 16:18:35 +0200

    created TypologySeeder

--- database/seeders/TypologySeeder.php
+++ database/seeders/TypologySeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
+use App\Models\Typology;
 class TypologySeeder extends Seeder
 {
     /**
@@ -12,6 +12,46 @@ class TypologySeeder extends Seeder
      */
     public function run(): void
     {
+        $restaurantTypes = [
+            [
+                'id' => 1,
+                'name' => 'Pizzeria',
+                'timestamps' => now(),
+            ],
+            [
+                'id' => 2,
+                'name' => 'Trattoria',
+                'timestamps' => now(),
+            ],
+            [
+                'id' => 3,
+                'name' => 'Ristorante di pesce',
+                'timestamps' => now(),
+            ],
+            [
+                'id' => 4,
+                'name' => 'Steakhouse',
+                'timestamps' => now(),
 
+            ],
+            [
+                'id' => 5,
+                'name' => 'Fast Food',
+                'timestamps' => now(),
+            ],
+            [
+                'id' => 6,
+                'name' => 'Sushi Bar',
+                'timestamps' => now(),
+            ],
+        ];
+
+        foreach($restaurantTypes as $restaurantType){
+            $typology = new Typology();
+            $typology ->id = $restaurantType['id'];
+            $typology ->name = $restaurantType['name'];
+            $typology ->timestamps= $restaurantType['timestamps'];
+            $typology ->save();
+        }
     }
 }
