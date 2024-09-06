3e939af94e4f6fd2a18d3deff693372fbcc395d3
MarcoSac1 <marcosaccone8@gmail.com>
2024-09-06 12:44:02 +0200

    fixed code issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            {{-- <a class="nav-link" href="{{ route('restourant') }}">{{ __('Restourant') }}</a> --}}
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
