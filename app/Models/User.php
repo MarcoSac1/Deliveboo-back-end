b3d05d121aca00d5f11bc0a89a893fea90a17098
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-09-06 11:21:32 +0200

    Fix restourant_typologies seed

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,8 +45,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function restourant_tipologies(){
-        return $this->belongsToMany(Typology::class , 'restourant_typology');
+    public function typologies()
+    {
+        return $this->belongsToMany(Typology::class, 'restourant_typologies', 'user_id', 'typology_id');
     }
 
     public function dishes(){
--- database/seeders/RestourantTypologySeeder.php
+++ database/seeders/RestourantTypologySeeder.php
@@ -8,14 +8,17 @@ use App\Models\Typology;
 use App\Models\User;
 class RestourantTypologySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        $users = User::all();
-        $typologies = Typology::all();
+    /**Run the database seeds.*/
+public function run(): void{
+    $users = User::all();
+    $typologies = Typology::all();
 
+        $users[0]->typologies()->attach($typologies[8]);
+        $users[2]->typologies()->attach($typologies[8]);
+        $users[3]->typologies()->attach($typologies[8]);
+        $users[1]->typologies()->attach($typologies[8]);
+        $users[1]->typologies()->attach($typologies[8]);
+        $users[1]->typologies()->attach($typologies[8]);
         // foreach($users as $userData) {
         //     $user = new User();
         //     $user->user_id = $userData['user_id'];
