526088dfe39d87f820b726858864f7d843fc5b1e
MarcoSac1 <marcosaccone8@gmail.com>
2024-09-06 16:20:26 +0200

    created show

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a href="{{ url('/restourant') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Restourant</a>
+                            <a href="{{ url('/restourant') }}" class=" font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 ">Restourant</a>
                             {{-- <a class="nav-link" href="{{ route('/restourant') }}">{{ __('Restourant') }}</a> --}}
                         </li>
                     </ul>
