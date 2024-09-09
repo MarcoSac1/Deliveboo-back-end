883232ad4d8a11e0daf15ad743a6f4cc53350a25
MarcoSac1 <marcosaccone8@gmail.com>
2024-09-09 11:58:13 +0200

    aligned item on index

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('main-section')
-<div>
-    <h1>Restourant</h1>
+<div class=" text-center">
+    <h1 class=" mb-4">Restourant</h1>
     @foreach ($users as $user)
-    <section class=" text-center px-3">
-            <div class="card col-4 mb-3 d-flex">
+    <section class="d-flex justify-content-around flex-wrap text-center justify-center px-3">
+            <div class="card col-4 mb-3 ">
                 <h1 class="card-header">{{ $user->username}}</h1>
                 <div class="card-body">
                     <p class="card-text"> foto: {{ $user->photo }} <br>
