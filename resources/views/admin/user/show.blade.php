5f6a38901654c4ddacba0b4852eb1c9da4f93bda
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-09-09 13:02:10 +0200

    `sISTEMATO CODICE

--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -8,14 +8,11 @@ Showing {{ $user->name }}
         <div class="col-8 p-3">
             <h1 class="card-header">{{ $user->username}}</h1>
             <div class="card-body">
-                <p class="card-text"> foto: {{ $user->photo }} <br>
+                <p class="card-text"> foto: {{ $user->photo }} </p>
                 <h5 class="card-title ">Indirizzo: {{ $user->adress }}</h5>
-                P.Iva: {{ $user->piva }} <br>
+                <p>P.Iva: {{ $user->piva }}</p>
                 <p>Mail: {{ $user->email }}</p>
-                {{-- <a href="{{ route('pages.show', $user) }}" class="btn btn-primary">Restourant detail</a> --}}
                 <a href="{{ route('pages.show', $user) }}" class="btn btn-primary">Restourant details</a>
-                {{-- <a href="{{ route('pages.edit', $user) }}" class="btn btn-success">Edit</a> --}}
-                {{-- <form action="{{ route('pages.destroy', $user) }}" method="POST" class="d-inline-block animal-destroy"> --}}
             </div>
         </div>
     </div>
