28301422aec990033caf554f26bfac3e51ee03d1
MarcoSac1 <marcosaccone8@gmail.com>
2024-09-09 10:59:50 +0200

    fixed route isssue

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,8 @@ use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\HomeController as GuestHomeController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Admin\DishController;
-use App\Http\Controllers\HomeController as GuestHomeController;
 
-use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +28,7 @@ Auth::routes();
 Route::get('/home', [GuestHomeController::class, 'index'])->name('home');
 
 Route::get('/restourant', [AdminHomeController::class,'index'])->name('pages.index');
+Route::get('/show', [AdminHomeController::class,'show'])->name('pages.show');
 
 Route::get('/create', [AdminHomeController::class,'create'])->name('pages.create');
 Route::post('/restourant', [AdminHomeController::class, 'store'])->name('pages.store');
