90240391141bd2e85582b692475a066820787da4
afablelorenz <afablelorenz@gmail.com>
2024-09-09 11:21:11 +0200

    updated code regarding dish index and route

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,10 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\OrderController;
-use App\Http\Controllers\HomeController as GuestHomeController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Admin\DishController;
 use App\Http\Controllers\HomeController as GuestHomeController;
 
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -38,6 +36,7 @@ Route::middleware('auth')->name('admin.')->prefix('admin/')->group(function(){
     // Route::get('secret-home', [AdminHomeController::class, 'index'])->name('home');
 
     Route::resource('orders', OrderController::class);
+    Route::resource('dishes', DishController::class);
 
     // Route::resource('/show',AdminHomeController::class);
 
